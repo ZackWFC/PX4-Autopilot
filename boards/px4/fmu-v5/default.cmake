78efd71a54472c1f0617625fcbc8867e7e72ccbf
wsk <wanskey@gmail.com>
2020-12-01 23:47:43 -0800

    XTW created

--- boards/px4/fmu-v5/default.cmake
+++ boards/px4/fmu-v5/default.cmake
@@ -24,6 +24,7 @@ px4_add_board(
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
+		ft205ev
 		gps
 		#heater
 		imu/adis16448
